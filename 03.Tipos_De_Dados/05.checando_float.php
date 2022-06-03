98857522c4554f92ec5962b797583f7b8d1cf103
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-03 10:53:14 -0300

    Verificando se o dado é float

---
+++ 03.Tipos_De_Dados/05.checando_float.php
@@ -0,0 +1,16 @@
+<?php
+
+    $a = "teste";
+    $b = 12.8;
+    
+    if (is_float($a)) {
+        echo "a variável 'a' é um float<br>";
+    }
+
+    if (is_float($b)) {
+        echo "a variável 'b' é um float<br>";
+    }
+
+    if (is_float(6565.63)) {
+        echo "6565.63 é um float<br>";
+    }
