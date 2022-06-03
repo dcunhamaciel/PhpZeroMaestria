2d7aea87a363cd118b1e46247fb6ecbe075cb699
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-03 09:45:47 -0300

    Palavras Reservadas

---
+++ 02.Conceitos_Basicos/06.palavras_reservadas.php
@@ -0,0 +1,15 @@
+<?php
+
+	// erro ao executar, echo é uma palavra reservada
+	// function echo() {
+	// 	return true;
+	// }
+
+	// erro ao executar, while é uma palavra reservada	
+	// function while() {
+	// 	return true;
+	// }
+
+	function whileNew() {
+		return true;
+	}
