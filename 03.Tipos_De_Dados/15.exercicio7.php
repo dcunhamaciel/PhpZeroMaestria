2a2b2be6326ad58b93ce080e4764edab6fc15677
dcunhamaciel <dcunhamaciel@gmail.com>
2022-06-04 10:05:13 -0300

    Exercício 7

---
+++ 03.Tipos_De_Dados/15.exercicio7.php
@@ -0,0 +1,12 @@
+<?php
+
+	$pessoa = [
+		"nome" => "Diego",
+		"idade" => 37,
+		"profissao" => "Programador",
+		"graduacao" => "Ciência da Computação"
+	];
+
+	if ($pessoa["idade"] >= 18) {
+		echo "A pessoa é maior de idade.";
+	}
