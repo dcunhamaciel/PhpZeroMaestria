652bcff89a87ee3a997503ebfab63e383366ff5b
dcunhamaciel <dcunhamaciel@gmail.com>
2022-06-04 11:53:18 -0300

    Declarando Variáveis

---
+++ 04.Variaveis/01.criacao_de_variaveis.php
@@ -0,0 +1,21 @@
+<?php
+
+    $teste = "Algum texto";
+
+    echo $teste;
+    echo "<br>";
+
+    $num = 10;
+
+    echo $num;
+    echo "<br>";
+
+    $_nome = "Diego";
+
+    echo $_nome;
+    echo "<br>";
+
+    $velocidadeMaxima = 100;
+
+    echo $velocidadeMaxima;
+    echo "<br>";
