fe22928c611f88dc780f324938866d2c3164b2e6
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-06 12:10:54 -0300

    Exercício 14

---
+++ 05.Expressoes_e_Operadores/11.exercicio14.php
@@ -0,0 +1,10 @@
+<?php
+
+	$saudacao = "Sr.";
+	$nome = "Pedro";
+	$sobrenome = "Amaral";
+
+	$frase = "O " . $saudacao . " " . $nome . " " . $sobrenome . ", vem hoje para a reunião às 14 horas.";
+	
+	echo $frase;
+	echo "<br>";
