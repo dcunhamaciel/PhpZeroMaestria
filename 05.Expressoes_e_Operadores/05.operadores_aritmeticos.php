bb4896964acfa6998ec647ccd5ec5cb594685d2f
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-06 10:08:58 -0300

    Operadores Aritméticos

---
+++ 05.Expressoes_e_Operadores/05.operadores_aritmeticos.php
@@ -0,0 +1,11 @@
+<?php
+
+	$soma = 5 + 5;
+	$subtracao = 10 - 2;
+	$multiplicacao = 2 * 199;
+	$divisao = 123 / 48;
+
+	echo $soma . "<br>";
+	echo $subtracao . "<br>";
+	echo $multiplicacao . "<br>";
+	echo $divisao . "<br>";
