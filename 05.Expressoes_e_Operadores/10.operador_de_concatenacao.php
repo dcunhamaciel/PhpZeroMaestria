0ef3f6fd6bfeace9e5bbb98a547939b8a80774b8
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-06 12:06:16 -0300

    Operador de Concatenação

---
+++ 05.Expressoes_e_Operadores/10.operador_de_concatenacao.php
@@ -0,0 +1,16 @@
+<?php
+
+	echo "testando" . " a " . "concatenação";
+	echo "<br>";
+	
+	$t = "testando";
+	$c = "concatenação";
+
+	echo $t . " a " . $c;
+	echo "<br>";
+
+	$marca = "Ferrari";
+	$motor = "3.0";
+	$velocidadeMaxima = 200;
+
+	echo "O carro da " . $marca . " tem um motor " . $motor . " e chega a uma velocidade máxima de " . $velocidadeMaxima . "km/h";
