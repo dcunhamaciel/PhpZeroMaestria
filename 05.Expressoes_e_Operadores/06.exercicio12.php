aca398632fee1d8ba61b2e2ef574459e659aef8c
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-06 10:59:18 -0300

    Exercício 12

---
+++ 05.Expressoes_e_Operadores/06.exercicio12.php
@@ -0,0 +1,17 @@
+<?php
+
+	$a = 3;
+	$b = 12;
+	$c = 12.4;
+	$d = 4.8;
+	$e = 9.2;
+
+	$operacao1 = $a - $b;
+	$operacao2 = $operacao1 * $c;
+	$operacao3 = $operacao2 + $d;
+	$operacao4 = $operacao3 / $e;
+
+	echo $operacao1 . "<br>";
+	echo $operacao2 . "<br>";
+	echo $operacao3 . "<br>";
+	echo $operacao4 . "<br>";
