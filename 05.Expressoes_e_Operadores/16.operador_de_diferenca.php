8e5ef110e8eada7de1351db53f02df17e5dc1a79
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-07 08:48:14 -0300

    Operadorde Diferença

---
+++ 05.Expressoes_e_Operadores/16.operador_de_diferenca.php
@@ -0,0 +1,20 @@
+<?php
+
+	$a = 3;
+	$b = 4;
+
+	if ($a != $b) {
+		echo "Testando diferença <br>";
+	}
+
+	if ($a != 3) {
+		echo "Testando diferença 2 <br>";
+	}
+
+	if (false != "teste") {
+		echo "Testando diferença 3 <br>";
+	}
+
+	if (3 != "3") {
+		echo "Testando diferença 4 <br>";
+	}
