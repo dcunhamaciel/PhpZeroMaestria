467eefcff979765e7ba085f804eb8dd1fd3dc9ba
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-07 08:05:03 -0300

    Operador de Igualdade

---
+++ 05.Expressoes_e_Operadores/13.operador_de_igualdade.php
@@ -0,0 +1,28 @@
+<?php
+
+	if (3 == 3) {
+		echo "Comparação verdadeira <br>";
+	}
+
+	if (3 == 4) {
+		echo "Comparação verdadeira <br>";
+	}
+
+	$a = 12;
+	$b = 12;
+	$c = 100;
+
+	if ($a == $b) {
+		echo "Comparação verdadeira <br>";
+	}
+
+	if ($a == $c) {
+		echo "Comparação verdadeira <br>";
+	}
+
+	$nome1 = "Diego";
+	$nome2 = "Diego";
+
+	if ($nome1 == $nome2) {
+		echo "O nome coincide <br>";
+	}
