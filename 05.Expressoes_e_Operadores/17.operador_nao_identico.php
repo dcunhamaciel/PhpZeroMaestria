eb118d551a43214ed74b1d4f2240ec223b2d1828
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-07 08:53:43 -0300

    Operador Não Idêntico

---
+++ 05.Expressoes_e_Operadores/17.operador_nao_identico.php
@@ -0,0 +1,16 @@
+<?php
+
+	$a = 1;
+	$b = "1";
+
+	if ($a != $b) {
+		echo "A é diferente a B <br>";
+	}
+
+	if ($a !== $b) {
+		echo "A é estritamente diferente a B <br>";
+	}
+
+	if (1 != "1") {
+		echo "Não é idêntico 1<br>";
+	}
