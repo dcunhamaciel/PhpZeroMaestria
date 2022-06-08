5299091e5cf0d7c47cc89b5f76f87b915ab6feed
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-08 08:07:02 -0300

    Exercício 18

---
+++ 05.Expressoes_e_Operadores/24.exercicio18.php
@@ -0,0 +1,17 @@
+<?php
+
+	if (12 < 5 || "João" === "João") {
+		echo "A comparação 1 é verdadeira <br>";
+	}
+
+	if (1 > 5 || 1) {
+		echo "A comparação 2 é verdadeira <br>";
+	}
+
+	if (20 === "20" && 51 >= 31) {
+		echo "A comparação 3 é verdadeira <br>";
+	}
+
+	if (20 == "20" && 51 >= 31) {
+		echo "A comparação 4 é verdadeira <br>";
+	}
