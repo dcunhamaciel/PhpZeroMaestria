5633442bb2f208078c20d56a5f83ab1848a22d1f
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-08 09:19:32 -0300

    Exercício 19

---
+++ 05.Expressoes_e_Operadores/27.exercicio19.php
@@ -0,0 +1,21 @@
+<?php
+
+	$a = (int) "testando";
+
+	echo $a;
+	echo "<br>";
+
+	$b = (int) 12.9;
+
+	echo $b;
+	echo "<br>";
+
+	$c = (int) true;
+
+	echo $c;
+	echo "<br>";
+
+	$d = (int) [1, 2, 3, 4];
+
+	echo $d;
+	echo "<br>";
