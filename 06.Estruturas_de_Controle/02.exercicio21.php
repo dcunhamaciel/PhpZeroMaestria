7169fa459a4b324bdac7a2f767a73ad47c47e100
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-09 08:30:55 -0300

    Exercício 21

---
+++ 06.Estruturas_de_Controle/02.exercicio21.php
@@ -0,0 +1,15 @@
+<?php
+
+	$a = 5;
+	$b = 2;
+
+	if ($a > $b) {
+		echo "Entrou no if 1 <br>";
+	}
+
+	$nome1 = "Diego";
+	$nome2 = "Pedro";
+
+	if ($nome1 != $nome2) {
+		echo "Entrou no if 2 <br>";	
+	}
