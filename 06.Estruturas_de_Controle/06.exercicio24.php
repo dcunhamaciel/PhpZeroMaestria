b9562bd234fee63cdc0fa3d86a0af0f32b65d8d0
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-09 12:05:50 -0300

    Exercício 24

---
+++ 06.Estruturas_de_Controle/06.exercicio24.php
@@ -0,0 +1,23 @@
+<?php
+
+	$a = 12;
+	$b = "Roda";
+	$c = [];
+
+	if (is_int($a)) {
+		echo "A é um inteiro <br>";
+	} else {
+		echo "A não é um inteiro <br>";
+	}
+
+	if (is_int($b)) {
+		echo "B é um inteiro <br>";
+	} else {
+		echo "B não é um inteiro <br>";
+	}
+
+	if (is_int($c)) {
+		echo "C é um inteiro <br>";
+	} else {
+		echo "C não é um inteiro <br>";
+	}
