b85b4e470caebab871e95c001e121dbd371fb9b7
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-09 11:11:51 -0300

    Exercício 22

---
+++ 06.Estruturas_de_Controle/03.exercicio22.php
@@ -0,0 +1,21 @@
+<?php
+
+	$idade1 = 16;
+	$idade2 = 18;
+	$idade2 = 26;
+
+	$maioridade = 18;
+
+	$msg = "Você é maior de idade <br>";
+
+	if ($idade1 >= $maioridade) {
+		echo "1 - " . $msg;
+	}
+
+	if ($idade2 >= $maioridade) {
+		echo "2 - " . $msg;
+	}
+
+	if ($idade3 >= $maioridade) {
+		echo "3 - " . $msg;
+	}
