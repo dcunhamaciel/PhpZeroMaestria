25b89316090a744a22c17faa581f873becbd307c
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-09 08:00:57 -0300

    Estrutura IF

---
+++ 06.Estruturas_de_Controle/01.estrutura_if.php
@@ -0,0 +1,20 @@
+<?php
+
+    if (5 > 2) {
+        echo "Deu certo! Entrou no if <br>";
+    }
+
+    if (2 >= 5) {
+        echo "Não vai entrar no if, porque deu <br>";
+    }
+
+    if (10 === 10 && 9 > 3) {
+        echo "Deu certo! Entrou no if 2 <br>";
+    }
+
+    $a = 10;
+    $b = 5;
+
+    if ($a >= $b) {
+        echo "Deu certo, entrou no if 3 <br>";
+    }
