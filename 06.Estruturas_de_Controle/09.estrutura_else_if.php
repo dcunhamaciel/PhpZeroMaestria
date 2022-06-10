c997383f2a9cd72ddcef8abf6e2e21443abafc43
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-10 12:57:09 -0300

    Estrutura ELSE IF

---
+++ 06.Estruturas_de_Controle/09.estrutura_else_if.php
@@ -0,0 +1,21 @@
+<?php
+
+    if (10 > 5) {
+        echo "Entrou no if <br>";
+    } else if (10 > 6) {
+        echo "Entrou no else if <br>";
+    }
+
+    if (10 < 5) {
+        echo "Entrou no if <br>";
+    } else if (10 > 6) {
+        echo "Entrou no else if <br>";
+    }
+
+    if (10 < 5) {
+        echo "Entrou no if <br>";
+    } else if (10 < 6) {
+        echo "Entrou no else if <br>";
+    } else {
+        echo "Entrou no else <br>";
+    }
