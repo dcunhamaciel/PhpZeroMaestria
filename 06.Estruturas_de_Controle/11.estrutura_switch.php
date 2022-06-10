2b98fc649fec9cf6721fcad11287599f3549285b
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-10 13:05:39 -0300

    Estrutura Switch

---
+++ 06.Estruturas_de_Controle/11.estrutura_switch.php
@@ -0,0 +1,17 @@
+<?php
+
+    $x = 0;
+
+    switch ($x) {
+        case 0:
+            echo "X é igual a 0 <br>";
+            break;
+        case 1:
+            echo "X é igual a 1 <br>";
+            break;
+        case 2:
+            echo "X é igual a 2 <br>";
+            break;            
+        default:
+            echo "X não é igual a nenhum dos valores mencionados <br>";        
+    }
