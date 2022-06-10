3c4d8fd0b7c6e3df62e16564592d931fe9e572f7
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-10 13:13:52 -0300

    Estrutura WHILE

---
+++ 07.Estruturas_de_Repeticao/01.estrutura_while.php
@@ -0,0 +1,17 @@
+<?php
+
+    $x = 0;
+
+    while ($x < 10) {
+        echo "x = $x <br>";
+
+        $x++;
+    }
+
+    $y = 10;
+
+    while ($y > 0) {
+        echo "y = $y <br>";
+
+        $y--;
+    }
