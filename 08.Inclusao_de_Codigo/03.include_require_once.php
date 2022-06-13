e7bf1d048f41dd0bd085a8b35e0975270134d545
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-13 08:44:51 -0300

    Include e Require Once

---
+++ 08.Inclusao_de_Codigo/03.include_require_once.php
@@ -0,0 +1,10 @@
+<?php
+
+    include_once "03.include_require_once_teste.php";
+    include_once "03.include_require_once_teste.php";
+
+    require_once "03.include_require_once_teste.php";
+
+?>
+
+<p>Testando código!</p>
---
+++ 08.Inclusao_de_Codigo/03.include_require_once_teste.php
@@ -0,0 +1,3 @@
+<?php
+
+    echo "Testando include/require once <br>";
