4d972176597f3160f208ba219550af5149d55076
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-13 10:05:03 -0300

    Chamando Função

---
+++ 09.Funcoes/02.chamando_funcao.php
@@ -0,0 +1,15 @@
+<?php
+
+    function soma()
+    {
+        $a = 5;
+        $b = 4;
+
+        echo $a + $b;
+        echo "<br>";
+    }
+
+    soma();
+    soma();
+
+    echo strtoupper("testando função");
