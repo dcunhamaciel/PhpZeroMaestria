f8c4b4771520239a25c64c12021f91fdbfe44860
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-13 11:04:03 -0300

    Parâmetros de Função

---
+++ 09.Funcoes/05.parametros_de_funcao.php
@@ -0,0 +1,10 @@
+<?php
+
+    function velocidadeMaxima($velocidade)
+    {
+        echo "O carro atinge a velocidade máxima de $velocidade km/h <br>";
+    }
+
+    velocidadeMaxima(200);
+    velocidadeMaxima(300);
+    velocidadeMaxima(400);
