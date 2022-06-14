9e93eb34e462e7b33d306efef2412c236ff2791a
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-14 09:11:52 -0300

    Valores de Escape

---
+++ 10.Strings/02.valores_de_escape.php
@@ -0,0 +1,11 @@
+<?php
+
+    header("Content-type: text/plain");
+
+    echo "Isso aqui vai ficar na primeira linha \n e isso na segunda linha";
+
+    echo "Testando o tab \t aqui \n";
+
+    echo "Barra invertida \\ \n";
+
+    echo "Imprimindo o dólar \$teste \n";
