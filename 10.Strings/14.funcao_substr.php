b142adc8c779c72edb9da41c913c50c5c6762c71
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-14 13:45:12 -0300

    Função Substring

---
+++ 10.Strings/14.funcao_substr.php
@@ -0,0 +1,12 @@
+<?php
+
+    $str = "Esta é a minha string";
+
+    $minha = substr($str, 10, 5);
+
+    echo $str . "<br>";
+    echo $minha . "<br>";
+
+    $novaString = substr($str, 10);
+
+    echo $novaString . "<br>";
