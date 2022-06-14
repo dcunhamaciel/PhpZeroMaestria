52b7a50ff5ade2f7f4e9baf0674d5f79ee8b57d8
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-14 08:41:00 -0300

    Var_Dump e Print_R

---
+++ 09.Funcoes/16.vardump_e_printr.php
@@ -0,0 +1,13 @@
+<?php
+
+    $array = [
+        "Teste",
+        123,
+        3442.2313,
+        true,
+        [1, 2, 3]        
+    ];
+
+    print_r($array);
+    echo "<br>";
+    var_dump($array);
