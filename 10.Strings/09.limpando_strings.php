3837517815647513eae0edc2bef4effd1c6edab9
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-14 10:07:25 -0300

    Limpando Strings

---
+++ 10.Strings/09.limpando_strings.php
@@ -0,0 +1,11 @@
+<?php
+
+    $str1 = "    Diego    ";
+    $strLimpa1 = trim($str1);
+    $strLimpa2 = ltrim($str1);
+    $strLimpa3 = rtrim($str1);
+    
+    echo "Esta é a string 1: $str1.<br>";
+    echo "Esta é a string limpa 1: $strLimpa1.<br>";
+    echo "Esta é a string limpa 2: $strLimpa2.<br>";
+    echo "Esta é a string limpa 3: $strLimpa3.<br>";
