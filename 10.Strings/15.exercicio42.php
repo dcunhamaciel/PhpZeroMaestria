4d94f021a10fa731568f471f2ac4c65490ab8854
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-14 13:49:47 -0300

    Exercício 42

---
+++ 10.Strings/15.exercicio42.php
@@ -0,0 +1,9 @@
+<?php
+
+    $frase = "Cadê o meu queijo? Ele estava aqui em cima.";
+    
+    $queijo = substr($frase, 12, 6);
+    $estava = substr($frase, 24, 6);
+
+    echo $queijo . "<br>";
+    echo $estava . "<br>";
