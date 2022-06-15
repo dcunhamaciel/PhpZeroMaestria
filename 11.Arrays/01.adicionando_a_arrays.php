5f0bd81b5d85aae2212d6c435c4a77f3e3dd857e
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-15 09:48:22 -0300

    Adicionando Valores a um Array

---
+++ 11.Arrays/01.adicionando_a_arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+    $array = [];
+    print_r($array);
+
+    $array[0] = 10;
+    $array[1] = 15;
+    print_r($array);
+
+    $array[5] = 25;
+    print_r($array);
+
+    $arrayAssociativo = [];
+    $arrayAssociativo["carro"] = "BMW";
+    print_r($arrayAssociativo);
