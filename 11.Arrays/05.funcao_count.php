06ce342cbb41bd632c38231639a0b95591187310
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-15 10:32:57 -0300

    Quantidade de Elementos de um Array

---
+++ 11.Arrays/05.funcao_count.php
@@ -0,0 +1,12 @@
+<?php
+
+    $array1 = [1, 2, 3];
+
+    echo count($array1) . "<br>";
+
+    $array2 = [
+        "nome" => "Diego",
+        "idade" => 37
+    ];
+
+    echo count($array2) . "<br>";
