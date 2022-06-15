6ca17a0de6850939e0f83597b372429d9133ed1b
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-15 10:46:38 -0300

    Array Multidimensional

---
+++ 11.Arrays/06.array_multidimensional.php
@@ -0,0 +1,14 @@
+<?php
+
+    $array = [
+        [1, 2, 3],
+        [2, 4, 6, 8]
+    ];
+
+    print_r($array);
+    echo "<br>";
+
+    echo $array[1][2] . "<br>";
+    
+    echo count($array) . "<br>";
+    echo count($array[0]) . "<br>";
