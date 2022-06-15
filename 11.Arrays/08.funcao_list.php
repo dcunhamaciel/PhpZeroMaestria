04826ae884042b4e825360cb3a2ca3d916df6e28
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-15 12:04:14 -0300

    Criando Variáveis com a Função List

---
+++ 11.Arrays/08.funcao_list.php
@@ -0,0 +1,9 @@
+<?php
+
+    $pessoa = ["Diego", 29, "Programador"];
+
+    list($nome, $idade, $profissao) = $pessoa;
+
+    echo $nome . "<br>";
+    echo $idade . "<br>";
+    echo $profissao . "<br>";
