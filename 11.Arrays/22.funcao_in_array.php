ab08388a962e161bb9151a89cc766702196f288c
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-17 14:04:39 -0300

    Buscando em Arrays

---
+++ 11.Arrays/22.funcao_in_array.php
@@ -0,0 +1,9 @@
+<?php
+
+    $array = ["banana", "maça", "batata", "limão", "mamão"];
+
+    if (in_array("batata", $array)) {
+        echo "Há o item batata no array <br>";
+    } else {
+        echo "Não há batata no array <br>";
+    } 
