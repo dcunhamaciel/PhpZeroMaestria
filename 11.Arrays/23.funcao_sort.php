b62fc2d72815c5f243a301949c61a4319b518da3
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-17 14:08:30 -0300

    Ordenação de Arrays

---
+++ 11.Arrays/23.funcao_sort.php
@@ -0,0 +1,14 @@
+<?php
+
+    $numeros = [2, 1, 334, 32, 123, 65, 38, 999, 12, 4];
+
+    sort($numeros);    
+    print_r($numeros);
+
+    rsort($numeros);
+    print_r($numeros);
+
+    $nomes = ["Maria", "Aaron", "João", "José", "Diego"];
+
+    sort($nomes);    
+    print_r($nomes);    
