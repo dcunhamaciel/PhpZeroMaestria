f07af29cbd9b4fdf95856775171f323c41fafb33
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-17 14:43:23 -0300

    Invertendo Arrays

---
+++ 11.Arrays/26.funcao_array_reverse.php
@@ -0,0 +1,11 @@
+<?php
+
+    $array1 = [1, 2, 3, 4, 5];
+
+    $arrayReverso1 = array_reverse($array1);
+    print_r($arrayReverso1);
+
+    $array2 = ["Diego", 12, true, [1, 2]];
+
+    $arrayReverso2 = array_reverse($array2);
+    print_r($arrayReverso2);
