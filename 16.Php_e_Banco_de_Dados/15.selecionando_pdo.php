2797e2eefff3e1a9d37ccbada6cc99318d01bc0e
Diego Maciel <dcunhamaciel@gmail.com>
2022-06-30 15:10:33 -0300

    Selecionando Dados com PDO

---
+++ 16.Php_e_Banco_de_Dados/15.selecionando_pdo.php
@@ -0,0 +1,22 @@
+<?php
+
+    $host = "localhost";
+    $db = "cursophp";
+    $user = "root";
+    $pass = "";
+
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    $id = 5;
+
+    $stmt = $conn->prepare("SELECT * FROM itens WHERE id > :id");
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    // $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    // print_r($data);
+
+    $itens = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($itens);
