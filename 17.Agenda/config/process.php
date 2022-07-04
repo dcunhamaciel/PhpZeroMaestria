cb45ba009d3e61d791659231af451b52449c9c71
Diego Maciel <dcunhamaciel@gmail.com>
2022-07-04 11:17:57 -0300

    Tela de Registro de Contato

--- 17.Agenda/config/process.php
+++ 17.Agenda/config/process.php
@@ -5,10 +5,30 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $query = "SELECT * FROM contacts";
+    $id;
 
-    $stmt = $conn->prepare($query);
+    if (!empty($_GET)) {
+        $id = $_GET["id"];
+    }
 
-    $stmt->execute();
+    // Retorna o dado de um contato
+    if (!empty($id)) {
+        $query = "SELECT * FROM contacts WHERE id = :id";
+  
+        $stmt = $conn->prepare($query);
+  
+        $stmt->bindParam(":id", $id);
+  
+        $stmt->execute();
+  
+        $contact = $stmt->fetch();
+    } else {
+        // Retorna todos os contatos
+        $query = "SELECT * FROM contacts";
 
-    $contacts = $stmt->fetchAll();
+        $stmt = $conn->prepare($query);
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
