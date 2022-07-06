bf621065145e4240b6854373979b6481082d028d
Diego Maciel <dcunhamaciel@gmail.com>
2022-07-06 08:55:07 -0300

    Inserindo Contatos na Agenda

--- 17.Agenda/config/process.php
+++ 17.Agenda/config/process.php
@@ -5,30 +5,67 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $id;
+    $data = $_POST;
 
-    if (!empty($_GET)) {
-        $id = $_GET["id"];
-    }
+    // MODIFICAÇÕES NO BANCO
+    if (!empty($data)) {
+        // Criar contato
+        if ($data["type"] === "create") {
 
-    // Retorna o dado de um contato
-    if (!empty($id)) {
-        $query = "SELECT * FROM contacts WHERE id = :id";
-  
-        $stmt = $conn->prepare($query);
-  
-        $stmt->bindParam(":id", $id);
-  
-        $stmt->execute();
-  
-        $contact = $stmt->fetch();
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+    
+            $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+    
+            $stmt = $conn->prepare($query);
+    
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+    
+            try {    
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato criado com sucesso!";
+        
+            } catch(PDOException $e) {
+                // erro na conexão
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }    
+        }
+
+        // Redirect HOME
+        header("Location:" . $BASE_URL . "../index.php");        
     } else {
-        // Retorna todos os contatos
-        $query = "SELECT * FROM contacts";
+        $id;
 
-        $stmt = $conn->prepare($query);
+        if (!empty($_GET)) {
+            $id = $_GET["id"];
+        }
 
-        $stmt->execute();
+        // Retorna o dado de um contato
+        if (!empty($id)) {
+            $query = "SELECT * FROM contacts WHERE id = :id";
+    
+            $stmt = $conn->prepare($query);
+    
+            $stmt->bindParam(":id", $id);
+    
+            $stmt->execute();
+    
+            $contact = $stmt->fetch();
+        } else {
+            // Retorna todos os contatos
+            $query = "SELECT * FROM contacts";
 
-        $contacts = $stmt->fetchAll();
+            $stmt = $conn->prepare($query);
+
+            $stmt->execute();
+
+            $contacts = $stmt->fetchAll();
+        }
     }
+
+    // FECHAR CONEXÃO
+    $conn = null;
