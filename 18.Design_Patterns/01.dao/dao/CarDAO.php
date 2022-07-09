f831658b50954f65afaeba6fa721a3d02fb61031
dcunhamaciel <dcunhamaciel@gmail.com>
2022-07-09 09:23:41 -0300

    Inserindo Dados com DAO

--- 18.Design_Patterns/01.dao/dao/CarDAO.php
+++ 18.Design_Patterns/01.dao/dao/CarDAO.php
@@ -4,8 +4,20 @@
 
     class CarDAO implements CarDAOInterface {
 
-        public function create(Car $car) {
+        private $conn;
 
+        public function __construct(PDO $conn) {
+            $this->conn = $conn;
+        }
+
+        public function create(Car $car) {
+            $stmt = $this->conn->prepare("INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)");
+
+            $stmt->bindParam(":brand", $car->getBrand());
+            $stmt->bindParam(":km", $car->getKm());
+            $stmt->bindParam(":color", $car->getColor());
+      
+            $stmt->execute();
         }
 
         public function findAll() {
