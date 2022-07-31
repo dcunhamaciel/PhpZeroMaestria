6c6ebca31860c7264c24f833bdfd06701b371450
dcunhamaciel <dcunhamaciel@gmail.com>
2022-07-31 11:36:45 -0300

    Setup do Projeto

--- 19.MovieStart/routes/web.php
+++ 19.MovieStart/routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
