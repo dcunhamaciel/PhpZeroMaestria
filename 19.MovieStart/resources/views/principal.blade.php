759515d2f4730e5fedbd2075bf26bb7ab271aff9
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-02 19:39:10 -0300

    Cabeçalho e Rodapé

--- 19.MovieStart/resources/views/principal.blade.php
+++ 19.MovieStart/resources/views/principal.blade.php
@@ -10,9 +10,67 @@
     <title>MovieStart</title>
 </head>
 <body>
-    <h1>Testando MovieStar</h1>
-    <i class="far fa-plus-square"></i>
-    
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a href="" class="navbar-brand">
+            <img src="{{ asset('img/logo.svg') }}" alt="MovieStar" id="logo">
+            <span id="moviestar-title">MovieStar</span>
+            </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="" class="nav-link">
+                            <i class="far fa-plus-square"></i>Incluir Filme
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Meus Filmes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link bold"></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Sair</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div id="main-container" class="container-fluid">
+        <h1>Corpo do site</h1>
+    </div>    
+    <footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar filme</a></li>
+                <li><a href="#">Adicionar crítica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
+            </ul>
+        </div>
+        <p>&copy; 2020 Hora de Codar</p>
+    </footer>    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.js" integrity="sha512-39Q5Yw4jtWPVkYu5Dry3HiRh6DWG4FDODb/rHF+X2Xp0kzhg9VgTyYV209uQ/EsqPDP/4dDvwm1rb8JgnAkHNg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
