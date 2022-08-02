6e60b1a459e787f060f324d637547e1e80894d01
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-02 19:49:31 -0300

    CSS do Cabeçalho e Rodapé

--- 19.MovieStart/resources/views/principal.blade.php
+++ 19.MovieStart/resources/views/principal.blade.php
@@ -20,10 +20,10 @@
                     <i class="fas fa-bars"></i>
                 </button>
             <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" placeholder="Buscar Filmes" aria-label="Search">
                 <button class="btn my-2 my-sm-0" type="submit">
                     <i class="fas fa-search"></i>
-                </button>
+                </button>                
             </form>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
